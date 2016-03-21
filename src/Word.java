8b21fcbd1122b490c43f072bd1400035c97e3b87
RomanK94 <kyznetsoffr@gmail.com>
2016-03-21 19:24:59 +0200

    Update Word, create constructor

---
+++ src/Word.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Word {
+    private static Word word;
+    private String answer;
+    private String question;
+    private HashMap<String, String> map = new HashMap<>();
+    private BufferedReader inputStream;
+
+    private Word() {
+        try {
+            inputStream = new BufferedReader(new FileReader("d:/Quiz/question-answer.txt"));
+            while (inputStream.ready()) {
+                String line = inputStream.readLine();
+                String[] text = line.split(" : ");
+                map.put(text[1],text[0]);
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("Data file with question isn't found");
+        } catch (IOException exception) {
+            System.out.println("Problem with Input Stream");
+        }
+
+        Random       random    = new Random();
+        List<String> keys      = new ArrayList<String>(map.keySet());
+        answer = keys.get( random.nextInt(keys.size()) );
+        question     = map.get(answer);
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public static Word getWord() {
+        if (word == null) {
+            word = new Word();
+        }
+        return word;
+    }
+
+}
+
