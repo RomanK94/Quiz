93c5e2b0b23ac49c5f1b0440cc501c2e0121a1ea
RomanK94 <kyznetsoffr@gmail.com>
2016-03-22 16:09:19 +0200

    added method's for generated word for Quiz

--- src/Word.java
+++ src/Word.java
@@ -13,11 +13,11 @@ public class Word {
 
     private Word() {
         try {
-            inputStream = new BufferedReader(new FileReader("d:/Quiz/question-answer.txt"));
+            inputStream = new BufferedReader(new FileReader("question-answer.txt"));
             while (inputStream.ready()) {
                 String line = inputStream.readLine();
                 String[] text = line.split(" : ");
-                map.put(text[1],text[0]);
+                map.put(text[1], text[0]);
             }
         } catch (FileNotFoundException exception) {
             System.out.println("Data file with question isn't found");
@@ -25,10 +25,10 @@ public class Word {
             System.out.println("Problem with Input Stream");
         }
 
-        Random       random    = new Random();
-        List<String> keys      = new ArrayList<String>(map.keySet());
-        answer = keys.get( random.nextInt(keys.size()) );
-        question     = map.get(answer);
+        Random random = new Random();
+        List<String> keys = new ArrayList<String>(map.keySet());
+        answer = keys.get(random.nextInt(keys.size()));
+        question = map.get(answer);
     }
 
     public String getQuestion() {
